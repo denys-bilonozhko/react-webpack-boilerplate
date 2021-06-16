02ea413396f8b056168b658b43f4066db91b938c
Denys Bilonozhko <deformix@gmail.com>
2021-06-16 13:16:22 +0300

    feat: add project files

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import './App.css';
+
+import Button from './components/Button';
+
+const App = () => {
+  const [toggle, setToggle] = useState(false);
+
+  const toggleText = () => {
+    setToggle(!toggle);
+  };
+  return (
+    <div>
+      <h1 className="title">Webpack</h1>
+      <Button toggle={toggleText}>{toggle ? 'Hide' : 'Show'}</Button>
+      {toggle && (
+        <>
+          <p>Simple Webpack boilerplate for React</p>
+          <span>Packages:</span>
+          <ul>
+            <li>webpack</li>
+            <li>webpack-cli</li>
+            <li>webpack-dev-server</li>
+            <li>html-webpack-plugin</li>
+            <li>mini-css-extract-plugin</li>
+            <li>@babel/core</li>
+            <li>@babel/preset-env</li>
+            <li>@babel/preset-react</li>
+            <li>babel-loader</li>
+            <li>css-loader</li>
+            <li>react</li>
+            <li>react-dom</li>
+          </ul>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default App;
